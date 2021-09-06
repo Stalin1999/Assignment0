7ce47a5130b3f2849720b9cdd78024e8a1e0aef3
Mads <mpia@itu.dk>
2021-09-06 15:15:17 +0200

    Git pushed tests but not everything else

--- LeapYear/LeapYear/Program.cs
+++ LeapYear/LeapYear/Program.cs
@@ -3,10 +3,21 @@
 namespace LeapYear
 {
     public class Program
-    {
+    {   
         public static void Main(string[] args)
         {
-        
+            Program program = new Program();
+            string input = Console.ReadLine();
+            int year = Int32.Parse(input);
+            bool leapYear = program.isLeapYear(year);
+
+            if (leapYear)
+            {
+                Console.WriteLine("yay");
+            } else 
+            {
+                Console.WriteLine("nay");
+            }
         }
 
         public bool isLeapYear(int year)
